0c983de31928a0c5b0bbc4dfd8e9f57e93b0c365
Raj Bhatia <raj2784@hotmail.com>
2020-02-23 21:37:22 +1030

    Importing project

---
+++ HealthAPI/Controllers/PatientsController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HealthAPI.Data;
+using HealthAPI.Models;
+using Microsoft.AspNetCore.Cors;
+
+namespace HealthAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [EnableCors("CorsOrigin")]
+    public class PatientsController : ControllerBase
+    {
+        private readonly HealthContext _context;
+
+        public PatientsController(HealthContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Patients
+        [HttpGet]
+        public IEnumerable<Patient> GetPatients()
+        {
+            return _context.Patients
+                .Include(a => a.Ailments)
+                .Include(m => m.Medications);
+        }
+
+        // GET: api/Patients/5
+        //[HttpGet("{id}")]
+        [HttpGet("GetPatientbyId")]
+        public async Task<IActionResult> GetPatient(int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var patient = await _context.Patients.Include(a => a.Ailments).Include(m => m.Medications).FirstOrDefaultAsync(i => i.PatientId == id);
+
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(patient);
+        }
+
+        // PUT: api/Patients/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPatient(int id, Patient patient)
+        {
+            if (id != patient.PatientId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(patient).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PatientExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Patients
+        [HttpPost]
+        public async Task<ActionResult<Patient>> PostPatient(Patient patient)
+        {
+            _context.Patients.Add(patient);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPatient", new { id = patient.PatientId }, patient);
+        }
+
+        // DELETE: api/Patients/5
+        //[HttpDelete("{id}")]
+        [HttpDelete("DeletePatientbyId")]
+        public IActionResult DeletePatient(int id)
+        {
+            var patient = _context.Patients.Include(a=>a.Ailments).Include(m=>m.Medications).FirstOrDefault(i => i.PatientId == id);
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            _context.Patients.Remove(patient);
+            _context.SaveChanges();
+
+            return Ok(patient);
+        }
+
+        [HttpGet("{id:int}/medication")]
+        public async Task<IActionResult> GetMedications(int id)
+        {
+            var patient = await _context.Patients.Include(m => m.Medications).FirstOrDefaultAsync(i => i.PatientId == id);
+
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(patient.Medications);
+        }
+
+        [HttpGet("{id:int}/ailment")]
+        public async Task<IActionResult> GetAilment(int id)
+        {
+            var patient = await _context.Patients.Include(a => a.Ailments).FirstOrDefaultAsync(i => i.PatientId == id);
+
+            if (patient == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(patient.Ailments);
+        }
+        private bool PatientExists(int id)
+        {
+            return _context.Patients.Any(e => e.PatientId == id);
+        }
+
+
+    }
+}
---
+++ HealthAPI/Startup.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HealthAPI.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace HealthAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<HealthContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DBConnection")));
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            //Add Cors (cross origin resource sharing)
+            services.AddCors(options => options.AddPolicy("CorsOrigin", builder =>
+               {
+                   builder.AllowAnyOrigin()
+                          .WithMethods("*")
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+               }));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            //app.UseCors();
+
+            DummyData.Initialize(app);
+        }
+    }
+}
